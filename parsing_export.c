f8a5ce87f03d83823f3be0c611593204d371aa2f
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2025-04-29 05:57:24 +0000

    checking the equal signe that gives the value to the variable in export

--- parsing_export.c
+++ parsing_export.c
@@ -61,13 +61,34 @@ void    check_var(t_token **token)
 	}
 }
 
+void    check_equal_sign(t_token **token, char *var)
+{
+	char	*equal;
+	int		i;
+	int		len;
+
+	equal = (*token)->value;
+	i = 0;
+	len = ft_strlen(equal);
+	if (len != 1 && equal[0] != '=')
+	{
+		ft_putstr_fd("export: not an identifier: ", 1);
+		ft_putstr_fd(var, 1);
+		exit (1);
+	}
+}
+
 void	ft_export(t_token **token)
 {
 	t_token	*token_tmp;
+	char	*var;
 
 	if ((!token) || (ft_strncmp((*token)->value, "export", 6) != 0))
 		return ;
 	token_tmp = (*token);
 	token_tmp = token_tmp->next;
 	check_var(&token_tmp);
+	var = token_tmp->value;
+	token_tmp = token_tmp->next;
+	check_equal_sign(&token_tmp, var);
 }
