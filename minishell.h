4cbc05140e552119f883edd62bf46a7bb12e09d7
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-06-28 01:42:18 +0100

    a new echo

--- minishell.h
+++ minishell.h
@@ -188,14 +188,6 @@ t_token_exc				*tokens_exc_handler(t_token *token);
 /*-------------------echo helpers-------------------*/
 void					echo(t_token **token, t_token_exc **command);
 void					echo_helper(int *i, t_token_exc **command);
-// void					echo_handle_empty_cmd(t_echo *echo_struct_tmp);
-// int						echo_handle_n_flag(t_token **token_tmp);
-// void					echo_write_char_by_char(t_token *token_tmp,
-// 							t_echo *echo_struct_tmp);
-// void					echo_process_args(t_token **token, t_token *token_tmp,
-// 							t_echo *echo_struct_tmp, t_env *env_list);
-// void					echo_process_main_logic(t_token **token, t_token *head,
-// 							t_echo *echo_struct_tmp, t_env *env_list);
 
 /*-------------------export-------------------*/
 int						export_internal(t_token *tokens, t_env **env_list);
